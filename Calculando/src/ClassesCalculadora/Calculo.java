ed924659334dfd45e8f731ea63ad04fa718d9d7f
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2023-09-20 21:21:13 -0300

    Att

--- Calculando/src/ClassesCalculadora/Calculo.java
+++ Calculando/src/ClassesCalculadora/Calculo.java
@@ -16,15 +16,14 @@ public class Calculo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Calculadora calc1 = new Calculadora();
-        calc1.div(10, 5);
-        System.out.println(calc1.getResultadoString());
-        calc1.mult(10, 5);
-        System.out.println(calc1.convertDectoBin(8));
-        System.out.println(calc1.getResultadoString());
-        System.out.println(calc1.convertBintoDec("101010"));
-        System.out.println(calc1.getResultadoString());
+        Retangulo ret1 = new Retangulo(2.03);
+        System.out.println(ret1.getQouR());
+        Retangulo ret2 = new Retangulo(2.03, 5);
+        System.out.println(ret2.getQouR());
+        System.out.println(ret1.getQouR());
+        
+ 
+        
     }
     
 }
--- Calculando/src/ClassesCalculadora/Retangulo.java
+++ Calculando/src/ClassesCalculadora/Retangulo.java
@@ -1,22 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ClassesCalculadora;
 
-/**
- *
- * @author Alunos
- */
-public class Retangulo {
-public Retangulo(double lados){
 
+
+public class Retangulo {
+private double lado1;
+private double lado2;
+private double lados;
+private double area;
+private double perimetro;
+private String mensagem;    
+    
+    
+public Retangulo(double lados){
+this.setLado(lados);
+this.mensagem = "Quadrado";
 } 
 
 public Retangulo(double lado1, double lado2){
 
+this.mensagem = "Retangulo";    
 } 
+
+private double area(){
+this.area = this.lado1 * this.lado2;
     
+return this.area;
+}
+
+private double perimetro(){
+this.perimetro = 
+        
+return this.perimetro;
+}
+
+public void setLado(double lados){
+if(){
+
+}
+
     
+this.lado1 = lados;
+}
+
+public double getLado(){
+
+return this.lado;
+}
+
+public double getArea(){
+
+return this.area();
+}
+
+public double getPerimetro(){
+
+return this.perimetro();
+}
+
+public String getQouR(){
+
+return this.mensagem;
+}
+
 }
