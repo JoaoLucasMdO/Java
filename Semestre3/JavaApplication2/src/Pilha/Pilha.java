93bd9c391636f9aa9265ce35b7d47899a3a921b4
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2024-02-15 22:05:09 -0300

    Att

---
+++ Semestre3/JavaApplication2/src/Pilha/Pilha.java
@@ -0,0 +1,41 @@
+package Pilha;
+
+public class Pilha {
+    Object[] pilha;
+    int topo = 0;
+    public boolean cheia;
+    
+    public Pilha(int tamanho){
+    pilha = new Object[tamanho];
+    }
+
+    public void empilha(Object elemento) {
+        pilha[topo] = elemento;
+        topo ++;
+    }
+
+    public boolean vazia() {
+        if(topo == 0)
+            return true;
+        else 
+            return false;   
+    }   
+
+    public Object verificaTopo() {
+        return pilha[topo-1];
+    }
+
+    public Object desempilha() {
+        topo--;
+       return pilha[topo-1]; 
+    }
+
+    public boolean cheia() {
+      if(topo == 10)
+          return true;
+      else
+          return false;
+    }
+    
+   
+}
---
+++ Semestre3/JavaApplication2/test/TestePilha.java
@@ -0,0 +1,50 @@
+import Pilha.Pilha;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TestePilha {
+    public static int tamanhoPilha = 10;
+    public static Pilha p;
+    
+    //Método para inicializar a pilha
+    @BeforeClass
+    public static void inicializaPilha(){
+    p = new Pilha(tamanhoPilha);
+    }
+    
+    //Método para empilhar um elemneto na Pilha
+    //A pilha irá trabalhar com Object
+    
+    
+    @Test
+    public void EmpilharUmObjeto(){
+    assertTrue(p.vazia());
+    p.empilha("Elemento 1");
+    assertFalse(p.vazia());
+    }
+    
+    @Test
+    public void EmpilharDoisObjetosDesempilha1(){
+    assertTrue(p.vazia());
+    p.empilha("Elemento 1");
+    p.empilha("Elemento 2");
+    Object retorno = p.desempilha();
+    assertEquals("Elemento 1", p.verificaTopo()); 
+    assertFalse(p.vazia());
+    }
+    
+    @Test
+    public void empilharDezElemento(){
+        assertTrue(p.vazia());
+        for(int i = 0; i < 10; i ++){
+            p.empilha(i);
+        }
+        assertFalse(p.vazia());
+        assertTrue(p.cheia());
+    }
+}
