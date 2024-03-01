86da69cd0daaed3a4201149b1d791209c904d3e8
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2024-02-29 22:22:53 -0300

    Att

--- Semestre3/HorasTrabalho/src/HorasTrabalhadas/HorasTrabalhadas.java
+++ Semestre3/HorasTrabalho/src/HorasTrabalhadas/HorasTrabalhadas.java
@@ -2,12 +2,45 @@ package HorasTrabalhadas;
 
 public class HorasTrabalhadas {
 
-    int total = 0;
     
-    public int calculaHoras(int diaInic, int horaInic, int diaFim,int horaFim) {
+    
+    public float calculaHoras(float horaInic, float horaFim) {
+        float total = 0;
         
         total = horaFim - horaInic;
         
+        if(horaFim < horaInic){
+            total += 24;
+        }
+        
+            if(total > 6){
+            total += 1.5f;
+        }
+        
+        
+        
+        if(horaInic <= 6){
+            total += (6 - horaInic) * 0.5;
+        }   else if(horaFim <= 6){
+                total += (6 - horaFim) * 0.5;
+            } 
+        
+        if(horaFim >= 22){
+            total += (horaFim - 22) * 0.5;
+        }   else if(horaInic >= 22){
+                total += (horaInic - 22) * 0.5;
+            }  
+        
+        if(horaInic <= 8){
+            total += (8 - horaInic) * 0.5;
+        }   else if(horaFim <= 8){
+                total += (8 - horaFim) * 0.5;
+            }  
+        
+        if(horaFim >= 18){
+            total += (horaFim - 18) * 0.5;
+        }
+              
         return total;
     }
     
--- Semestre3/HorasTrabalho/test/TesteHorasTrabalho.java
+++ Semestre3/HorasTrabalho/test/TesteHorasTrabalho.java
@@ -14,10 +14,18 @@ public class TesteHorasTrabalho {
   @Test
   public void receberDuasDatasRetornarValorTotalDeHoras(){
     h = new HorasTrabalhadas();
-      assertEquals(7, h.calculaHoras(15, 20, 16, 3));
+      assertEquals(4f, h.calculaHoras(8f,12f), 0);
+      assertEquals(5f, h.calculaHoras(13f,18f), 0);
+      assertEquals(11.5f, h.calculaHoras(8f,18f), 0);
+      assertEquals(5.5f,  h.calculaHoras(14f,19f), 0);
+      assertEquals(7.0f,  h.calculaHoras(6f,12f), 0);
+      assertEquals(9.5f,  h.calculaHoras(6f,13f), 0);
+      assertEquals(9.0f,  h.calculaHoras(17f,23f), 0);
+      assertEquals(17.5f, h.calculaHoras(0f,09f), 0);
+      assertEquals(8f,  h.calculaHoras(22f,2f), 0);
+      assertEquals(19.5f,  h.calculaHoras(14f,2f), 0);
+      
       
   }
   
-    
-    
 }
